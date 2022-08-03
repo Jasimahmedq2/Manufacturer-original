f2c1c43e2597c06252f21ad33e62e710ceab47b3
jasimahmedq2 <jasimahmed90852417>
2022-08-03 23:46:56 +0600

    last-commit-assingment

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
   ">
   <div className="hero-content flex-col lg:flex-row-reverse  bg-gradient-to-r from-green-400 to-blue-500 hover:from-secondary hover:to-primary">
     <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold text-secondary">WELCOM TO  NAME!</h1>
+      <h1 className="text-5xl font-bold text-secondary">WELCOM TO  Bits Computer</h1>
       <p className="py-6 text-white text-xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ">
--- src/Page/Share/Nav.js
+++ src/Page/Share/Nav.js
@@ -14,6 +14,8 @@ const Nav = () => {
   }
 
   const navBar = <>
+    <li><Link to="/">Home</Link></li>
+
     <li><Link to="blog">Blog</Link></li>
     {
       user && <li><Link to='/dashboard'>dashboard</Link></li>
@@ -37,7 +39,7 @@ const Nav = () => {
             {navBar}
           </ul>
         </div>
-        <Link to="/" className="btn btn-ghost normal-case text-xl text-primary">Manufacturing</Link>
+        <Link to="/" className="btn btn-ghost normal-case text-xl text-primary">Bits Computer</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
@@ -46,9 +48,9 @@ const Nav = () => {
       </div>
       <div className="navbar-end">
 
-      <label tabIndex="1" htmlFor='my-drawer-2' className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
+        <label tabIndex="1" htmlFor='my-drawer-2' className="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
       </div>
     </div>
   );
